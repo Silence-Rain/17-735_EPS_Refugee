77957d5356b596f090f4bde064f81ed385cb8635
Shaohuang(Daniel) Mo <daniel.s.mo503@gmail.com>
2020-02-12 00:40:41 -0500

    Delete config.py

--- TODO_app/server/config.py
+++
@@ -1,8 +0,0 @@
-# Config file. In final project it should not be available on public repo
-
-# Database connection params
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWD = "Silence_0617"
-DB_PORT = 3306
-DB_DB = "eps_todo"
