d1fab172d60f57e158c45fb35a32d43f5eacef4f
Silence-Rain <daniel.s.mo503@gmail.com>
2020-04-09 02:55:06 -0400

    add styles for refugee status

--- nu-home/src/components/RefugeeStatus/index.js
+++ nu-home/src/components/RefugeeStatus/index.js
@@ -1,9 +1,61 @@
 import React from "react";
+import { Table, Popconfirm, Divider } from 'antd';
+
+const columns = [
+  {
+    title: 'Username',
+    dataIndex: 'username',
+    key: 'username',
+  },
+  {
+    title: 'Government ID',
+    key: 'id',
+    render: (text, record) => (
+      <span>
+      	{record.id ? <a>Download</a> : "Not Uploaded"}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a>Chat</a>
+        <Divider type="vertical"/>
+        <Popconfirm title={`Confirm the identity of ${record.username}?`}>
+          <a>Verify</a>
+        </Popconfirm>
+      </span>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    username: 'John Brown',
+    id: true
+  },
+  {
+    key: '2',
+    username: 'Silence',
+    id: false
+  },
+  {
+    key: '3',
+    username: 'Daniel',
+    id: true
+  },
+];
 
 class RefugeeStatus extends React.Component {
   render () {
     return (
-      <div>RefugeeStatus </div>
+      <div>
+      	RefugeeStatus 
+      	<Table columns={columns} dataSource={data} />
+      </div>
     )
   }
 }
